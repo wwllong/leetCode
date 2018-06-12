24c30417065b01610dcf5b14854098b1abb56575
Wwllong <wwllong97@outlook.com>
2018-06-12 19:56:29 +0800

    从排序数组中删除重复项

---
+++ primary/test_arrays/TestDeleteRepet.java
@@ -0,0 +1,19 @@
+package primary.test_arrays;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import primary.arrays.DeleteRepet;
+
+class TestDeleteRepet {
+
+	@Test
+	void test() {
+		int[] arr = {0,0,1,1,1,2,2,3,3,4};
+		int len = DeleteRepet.removeDuplicates(arr);
+		int[] toArr = new int[len];
+		System.arraycopy(arr, 0, toArr, 0, len);
+		Assert.assertArrayEquals(new int[] {0,1,2,3,4},toArr);
+	}
+
+}

09edf8e2f9db7802a5c719ede4a2c5faef0dc88e
Wwllong <wwllong97@outlook.com>
2018-10-02 15:53:27 +0800

    回文数字

---
+++ temp/IsPalindrome.java
@@ -0,0 +1,20 @@
+package temp;
+
+/**
+ * @author Wwl
+ * 回文数字
+ */
+public class IsPalindrome {
+	
+	/* 思路1：将数字转换为字符数组，倒转
+	 * */
+	public int method1(int x) {
+		int result = 0;
+		char[] cArray = Integer.toString(x).toCharArray();
+		for(int x1=cArray.length-1; x1>0; --x1) {
+			
+		}
+		
+		return result;
+	}
+}
---
+++ temp/IsPalindromeTest.java
@@ -0,0 +1,18 @@
+package temp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IsPalindromeTest {
+	int test1 = 121;
+	int test2 = -121;
+	int test3 = 10;
+	
+
+	@Test
+	void test() {
+		
+	}
+
+}

f36a4af6bc4b8610ce4cdd5ab929028d3f5baa3d
wenwl <wwllong18@163.com>
2021-12-31 01:34:28 +0800

    feat：Q70-爬楼梯

---
+++ src/main/java/com/wenwl/algorithm/leetcode/easy/ClimbingStairs.java
@@ -0,0 +1,13 @@
+package com.wenwl.algorithm.leetcode.easy;
+
+/**
+ * @author wenwl
+ * @see <a href="https://leetcode-cn.com/problems/climbing-stairs/">Q70-爬楼梯</a>
+ */
+public class ClimbingStairs {
+
+    public int climbStairs1(int n) {
+        
+    }
+
+}
---
+++ src/test/java/com/wenwl/algorithm/test/easy/ClimbingStairsTest.java
@@ -0,0 +1,21 @@
+package com.wenwl.algorithm.test.easy;
+
+import com.wenwl.algorithm.leetcode.easy.ClimbingStairs;
+import org.junit.Test;
+
+public class ClimbingStairsTest {
+
+    private ClimbingStairs climbingStairs = new ClimbingStairs();
+
+    private int n1 = 2;
+    private int expected1 = 2;
+
+    private int n2 = 3;
+    private int expected2 = 3;
+
+    @Test
+    public void test1(){
+
+    }
+
+}
